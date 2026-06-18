5f488f6c987bdde6d28ed96882d258e2d72adb09
ratann31 <ratansaurav31@gmail.com>
2026-06-18 16:13:33 +0530

    Fix CORS for Render frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,20 +6,19 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://interview-ai-yt-1-ud6e.onrender.com"
+    ],
     credentials: true
 }))
 
-/* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
