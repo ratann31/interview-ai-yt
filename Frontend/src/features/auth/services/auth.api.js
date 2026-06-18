dfcd7fb4ef5443e6c985032a5c1a62942d977a83
ratann31 <ratansaurav31@gmail.com>
2026-06-18 16:06:35 +0530

    Connect frontend to render backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-yt-23jy.onrender.com",
     withCredentials: true
 })
 
